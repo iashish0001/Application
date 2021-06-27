e83353b1c2467b004d5a2de59c5970c0816a4494
iashish0001 <iashish0001@users.noreply.github.com>
2021-06-27 11:19:46 +0530

    Initial commit

---
+++ app/src/main/java/com/iashish0001/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iashish0001.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String kjuh = "jdfa hfjkhjk kfhkh fkjhasjkhf kjas ksh fkhsajkh fjash jklf hh fjahsjlfhga fjkasgkj fgkasj";
+    }
+}
